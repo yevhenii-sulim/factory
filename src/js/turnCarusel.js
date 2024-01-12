231e5fdfe2f0e082dcfe7dafb89c641ca7620f67
evgeniy210191 <bees127@ukr.net>
2024-01-13 01:07:07 +0200

    add transition

--- src/js/turnCarusel.js
+++ src/js/turnCarusel.js
@@ -1,13 +1,32 @@
+const timeout = 250;
 export function turnCaruselPrev(box, witdthBox) {
   let count = box.offsetLeft;
   if (box.offsetLeft >= 0) {
-    box.style.left =
-      -(box.childElementCount - 1) * witdthBox.offsetWidth + 'px';
+    box.style = `
+      opacity:0
+    `;
+    setTimeout(
+      () =>
+        (box.style = `
+      left:${-(box.childElementCount - 1) * witdthBox.offsetWidth}px;
+      opacity:1
+    `),
+      timeout
+    );
     return;
   }
   if (count % witdthBox.offsetWidth === 0) {
-    count += witdthBox.offsetWidth;
-    box.style.left = count + 'px';
+    box.style = `
+    left:${count}px;
+      opacity:0
+    `;
+    setTimeout(() => {
+      count += witdthBox.offsetWidth;
+      return (box.style = `
+      left:${count}px;
+      opacity:1
+    `);
+    }, timeout);
   }
   position = Math.abs(count) / witdthBox.offsetWidth;
 }
@@ -15,11 +34,31 @@ export function turnCaruselPrev(box, witdthBox) {
 export function turnCaruselNext(box, witdthBox) {
   let count = box.offsetLeft;
   if (box.offsetLeft <= -(box.childElementCount - 1) * witdthBox.offsetWidth) {
-    box.style.left = 0 + 'px';
+    box.style = `
+    left:${count}px;
+    opacity:0;
+    `;
+    setTimeout(
+      () =>
+        (box.style = `
+      left:0px;
+      opacity:1
+    `),
+      timeout
+    );
     return;
   }
   if (count % witdthBox.offsetWidth === 0) {
-    count -= witdthBox.offsetWidth;
-    box.style.left = count + 'px';
+    box.style = `
+    left:${count}px;
+    opacity:0;
+    `;
+    setTimeout(() => {
+      count -= witdthBox.offsetWidth;
+      box.style = `
+      left:${count}px;
+      opacity:1
+    `;
+    }, timeout);
   }
 }
